9799788e1c7a4f31833acbee7cfce24bec92e99a
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-03 09:40:37 -0500

    added security module outline

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SecurityModule } from './security/security.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SecurityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/ui/src/app/security/security.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SecurityRoutingModule } from './security-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SecurityRoutingModule
+  ],
+  declarations: []
+})
+export class SecurityModule { }
